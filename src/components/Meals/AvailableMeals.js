a24bcb115f6ba8015aabd15f9856a24c6f11761b
Trish Bonnette <trish.dev7@gmail.com>
2023-07-10 16:04:38 -0500

    Add files via upload
    or1iginal commit

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,64 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 14.99,
+  },
+  {
+    id: 'm3',
+    name: 'Veggie Pad Thai',
+    description: 'Thai food yum',
+    price: 11.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Impossible Bowl',
+    description: 'Healthy...and green with  protein...',
+    price: 16.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
